a24766fd7a65ed73a37d8a3c731e45d76d01a60b
Susan Subedi <ssubedi1@go.olemiss.edu>
2020-09-17 18:42:16 -0500

    Done with page transition and the donation page

---
+++ src/components/Button.js
@@ -0,0 +1,7 @@
+import React from "react";
+import "./Button.scss";
+const Button = () => {
+  return <button className="button">Donate</button>;
+};
+
+export default Button;
--- src/containers/Donation.js
+++ src/containers/Donation.js
@@ -1,17 +1,55 @@
-import React from "react";
+import React, { useState } from "react";
 import * as Icons from "react-feather";
+import Button from "../components/Button";
 
-const ImageDiv = () => {
-  return <div className="donation_image_div"></div>;
+const ImageDiv = ({ height }) => {
+  return (
+    <div
+      style={{
+        height,
+      }}
+      className="donation_image_div"
+    >
+      <div className="go-back">
+        <Icons.ChevronLeft />
+        <span>Back</span>
+      </div>
+      <img src="https://physicsworld.com/wp-content/uploads/2019/09/PWSep19Nemzer-frontis_HERO-1024x576.jpg" />
+    </div>
+  );
 };
 
-const FloatingDiv = () => {
+const FloatingDiv = ({ top, setHeight }) => {
+  const Enum = {
+    Story: 1,
+    Updates: 2,
+    Comments: 3,
+  };
+  const [content, setContent] = useState(Enum.Story);
+
+  const handleContentClick = (num) => {
+    if (!top) {
+      setHeight();
+    }
+
+    setContent(num);
+  };
   return (
     <div className="floating_div">
       <div className="bookmark">
         <Icons.Bookmark size="3rem" color="white" fill="white" />
       </div>
-      <div className="dash"></div>
+      <div
+        style={{
+          transform: `${!top ? "rotateY(180deg)" : ""}`,
+        }}
+        onClick={() => {
+          setHeight();
+        }}
+        className="dash"
+      >
+        <Icons.ChevronUp />
+      </div>
       <div className="types">
         <span className="type">Medical</span>
         <span className="type">Emergency</span>
@@ -48,25 +86,66 @@ const FloatingDiv = () => {
           <img src="https://upload.wikimedia.org/wikipedia/commons/3/32/Pulitzer2018-portraits-kendrick-lamar.jpg" />
           <div>99+</div>
         </div>
-        <div className="content-title">
-          <span className="story active">Story</span>
-          <span className="updates">
-            Updates <span className="grey-out">(13)</span>
-          </span>
-          <span className="comments">
-            Comments <span className="grey-out">(69)</span>
-          </span>
-        </div>
-        <div className="main-content"></div>
+      </div>
+      <div className="content-title">
+        <span
+          onClick={() => handleContentClick(Enum.Story)}
+          className={`story ${content === Enum.Story ? "active" : ""}`}
+        >
+          Story
+        </span>
+        <span
+          onClick={() => handleContentClick(Enum.Updates)}
+          className={`updates ${content === Enum.Updates ? "active" : ""}`}
+        >
+          Updates <span className="grey-out">(13)</span>
+        </span>
+        <span
+          onClick={() => handleContentClick(Enum.Comments)}
+          className={`updates ${content === Enum.Comments ? "active" : ""}`}
+        >
+          Comments <span className="grey-out">(69)</span>
+        </span>
+      </div>
+
+      <div className="main-content">
+        Lorem Ipsum Lorem IpsumLorem IpsumLorem Ipsum Lorem Ipsum Lorem Ipsum
+        Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum
+        Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum
+        Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum
+        Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum
+        Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum
+        Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum
+        Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum
+        Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum
+        Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum
+        Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum
+        Lorem Ipsum Lorem Ipsum Lorem Ipsum
       </div>
     </div>
   );
 };
+
+const ButtonContainer = () => (
+  <div className="bottom-buttons">
+    <div className="share">
+      <Icons.Share />
+    </div>
+    <Button />
+  </div>
+);
+
 const Donation = () => {
+  const [top, setTop] = useState(false);
+  const handleHeightChange = () => {
+    setTop(!top);
+  };
+  const height = top ? "20vh" : "40vh";
   return (
     <div className="Donation">
-      <ImageDiv />
-      <FloatingDiv />
+      <ImageDiv height={height} />
+      <FloatingDiv top={top} setHeight={handleHeightChange} />
+      <ButtonContainer />
     </div>
   );
 };
