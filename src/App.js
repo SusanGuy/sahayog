942af932496ccb940d5422364e62fc564bb7fad3
sushantbaskota2 <sushantbaskota2@gmail.com>
2020-09-18 15:59:50 -0500

    Added Slider

--- src/App.js
+++ src/App.js
@@ -1,13 +1,14 @@
-import React from "react";
-import "./App.scss";
-import Donation from "./containers/Donation";
+import React from 'react';
+import './App.scss';
+import Donation from './containers/Donation';
+import Donate from './containers/Donate';
 
 function App() {
-  return (
-    <div className="App">
-      <Donation />
-    </div>
-  );
+    return (
+        <div className='App'>
+            <Donate />
+        </div>
+    );
 }
 
 export default App;
--- src/containers/Donate.js
+++ src/containers/Donate.js
@@ -1,7 +1,29 @@
-import React from 'react';
+import React, { useState } from 'react';
+// import SliderWrapper from '../components/Slider';
 
 const Donate = () => {
-    return <div />;
+    const [ donation, setdonation ] = useState(0);
+
+    const handleDonation = ({ target }) => {
+        setdonation(target.value);
+    };
+    return (
+        <div className='donation-wrapper'>
+            <div className='donation-fill' style={{ width: `${donation}%` }} />
+            <div>
+                <img />
+            </div>
+            <SliderWrapper donation={donation} changeDonation={handleDonation} />
+        </div>
+    );
+};
+
+const SliderWrapper = ({ donation, changeDonation }) => {
+    return (
+        <div style={{ zIndex: 4 }}>
+            <input className='slider' type='range' min='0' max='100' onChange={changeDonation} value={donation} />
+        </div>
+    );
 };
 
 export default Donate;
