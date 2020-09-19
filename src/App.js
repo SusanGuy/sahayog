f8d5d5d581968fb720d7d5e3f6a5b8849e0f1c8b
sushantbaskota2 <sushantbaskota2@gmail.com>
2020-09-19 18:22:34 -0500

    Fixed issues

--- src/App.js
+++ src/App.js
@@ -3,13 +3,15 @@ import './App.scss';
 import Campaign from './containers/Campaign';
 
 import Donate from './containers/Donate';
+import Payment from './containers/Payment';
 
 function App() {
     return (
         <div className='App'>
             {
                 // <Donate target={25000} />
-                <Campaign />
+                // <Campaign />
+                <Payment />
             }
         </div>
     );
