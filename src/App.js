75d61432bb9a2e534e9a8eeaa403039108ce554a
Susan Subedi <ssubedi1@go.olemiss.edu>
2020-09-18 23:39:41 -0500

    Fixed viewport issue

--- src/App.js
+++ src/App.js
@@ -1,14 +1,13 @@
-import React from 'react';
-import './App.scss';
-import Donation from './containers/Donation';
-import Donate from './containers/Donate';
+import React from "react";
+import "./App.scss";
+import Campaign from "./containers/Campaign";
 
 function App() {
-    return (
-        <div className='App'>
-            <Donate />
-        </div>
-    );
+  return (
+    <div className="App">
+      <Campaign />
+    </div>
+  );
 }
 
 export default App;
