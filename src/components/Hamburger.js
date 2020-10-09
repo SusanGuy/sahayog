b3352794941c8d9566e6088eb24d8369f90eef30
sushantbaskota2 <sushantbaskota2@gmail.com>
2020-10-09 11:55:33 -0500

    Added MyDonations and Myfavorites page

--- src/components/Hamburger.js
+++ src/components/Hamburger.js
@@ -1,116 +1,100 @@
-import React, { useState, useRef, useEffect, useCallback } from "react";
-import * as Icons from "react-feather";
-import AuthButton from "./AuthButton";
-import { motion } from "framer-motion";
-import "./Hamburger.scss";
-import { Link } from "react-router-dom";
+import React, { useState, useRef, useEffect, useCallback } from 'react';
+import * as Icons from 'react-feather';
+import AuthButton from './AuthButton';
+import { motion } from 'framer-motion';
+import './Hamburger.scss';
+import { Link } from 'react-router-dom';
 const menus = {
-  HOME: { icon: <Icons.Home />, title: "Home", link: "/" },
-  DONATION: {
-    icon: <Icons.Plus />,
-    title: "My Donations",
-    link: "/my-donations",
-  },
-  CONTRIBUTION: {
-    icon: <Icons.Smile />,
-    title: "My Fundraisers",
-    link: "/my-fundraisers",
-  },
-  FAVORITE: {
-    icon: <Icons.Star />,
-    title: "My Favorites",
-    link: "/my-favorites",
-  },
+    HOME: { icon: <Icons.Home />, title: 'Home', link: '/' },
+    DONATION: {
+        icon: <Icons.Plus />,
+        title: 'My Donations',
+        link: '/my-donations'
+    },
+    CONTRIBUTION: {
+        icon: <Icons.Smile />,
+        title: 'My Fundraisers',
+        link: '/my-fundraisers'
+    },
+    FAVORITE: {
+        icon: <Icons.Star />,
+        title: 'My Favorites',
+        link: '/my-favorites'
+    },
 
-  PROFILE: {
-    icon: <Icons.Headphones />,
-    title: "My Profile",
-    link: "/my-profile",
-  },
+    PROFILE: {
+        icon: <Icons.Headphones />,
+        title: 'My Profile',
+        link: '/my-profile'
+    }
 };
 const Hamburger = ({ hamburger, handleBurger, route }) => {
-  const [active, setactive] = useState(route);
-  const node = useRef();
-  const handleClick = useCallback(
-    (e) => {
-      if (node.current.contains(e.target)) {
-        return;
-      }
-      handleBurger(false);
-    },
-    [handleBurger]
-  );
-  useEffect(
-    (_) => {
-      window.addEventListener("click", handleClick);
-      return () => {
-        document.removeEventListener("click", handleClick);
-      };
-    },
-    [handleClick]
-  );
+    const [ active, setactive ] = useState(route);
 
-  return (
-    <motion.div
-      animate={{ x: 0 }}
-      initial={{ x: "-100%" }}
-      exit={{ x: "-100%" }}
-      transition={{ duration: 0.3 }}
-      className="hamburger-container"
-    >
-      <div ref={node} className="hamburger-menu">
-        <div
-          className="hamburger-cross"
-          onClick={() => {
-            handleBurger(false);
-          }}
-        >
-          <Icons.X />
-        </div>
+    return (
         <motion.div
-          animate={{ y: 0 }}
-          initial={{ y: -2000 }}
-          transition={{ duration: 0.4 }}
-          exit={{ y: -2000 }}
-          className="hamburger-top"
+            animate={{ x: 0 }}
+            initial={{ x: '-100%' }}
+            exit={{ x: '-100%' }}
+            transition={{ duration: 0.3 }}
+            className='hamburger-container'
+            onClick={(e) => {
+                e.stopPropagation();
+            }}
         >
-          <div className="img-wrapper">
-            <img src="https://www.unh.edu/unhtoday/sites/default/files/styles/article_huge/public/article/2019/professional_woman_headshot.jpg?itok=3itzxHXh" />
-          </div>
-          <span>Sushant Baskota</span>
+            <div className='hamburger-menu'>
+                <div
+                    className='hamburger-cross'
+                    onClick={() => {
+                        handleBurger(false);
+                    }}
+                >
+                    <Icons.X />
+                </div>
+                <motion.div
+                    animate={{ y: 0 }}
+                    initial={{ y: -2000 }}
+                    transition={{ duration: 0.4 }}
+                    exit={{ y: -2000 }}
+                    className='hamburger-top'
+                >
+                    <div className='img-wrapper'>
+                        <img src='https://www.unh.edu/unhtoday/sites/default/files/styles/article_huge/public/article/2019/professional_woman_headshot.jpg?itok=3itzxHXh' />
+                    </div>
+                    <span>Sushant Baskota</span>
+                </motion.div>
+                <motion.div
+                    animate={{ y: 0 }}
+                    initial={{ y: 2000 }}
+                    exit={{ y: 2000 }}
+                    transition={{ duration: 0.5 }}
+                    className='hamburger-items'
+                >
+                    {Object.keys(menus).map((key) => {
+                        const { title, icon, link } = menus[key];
+                        return (
+                            <div
+                                key={link}
+                                onClick={() => {
+                                    handleBurger(false);
+                                    setactive(link);
+                                }}
+                                className={`hamburger-item ${active === link ? 'active' : ''}`}
+                            >
+                                <Link to={link}>
+                                    {icon}
+                                    <span>{title}</span>
+                                </Link>
+                            </div>
+                        );
+                    })}
+                    <div className='authentication-status'>
+                        <AuthButton>Log Out</AuthButton>
+                    </div>
+                </motion.div>
+            </div>
         </motion.div>
-        <motion.div
-          animate={{ y: 0 }}
-          initial={{ y: 2000 }}
-          exit={{ y: 2000 }}
-          transition={{ duration: 0.5 }}
-          className="hamburger-items"
-        >
-          {Object.keys(menus).map((key) => {
-            const { title, icon, link } = menus[key];
-            return (
-              <div
-                key={link}
-                onClick={() => {
-                  handleBurger(false);
-                  setactive(link);
-                }}
-                className={`hamburger-item ${active === link ? "active" : ""}`}
-              >
-                <Link to={link}>
-                  {icon}
-                  <span>{title}</span>
-                </Link>
-              </div>
-            );
-          })}
-          <div className="authentication-status">
-            <AuthButton>Log Out</AuthButton>
-          </div>
-        </motion.div>
-      </div>
-    </motion.div>
-  );
+    );
 };
 
 export default Hamburger;
---
+++ src/containers/MyDonations.js
@@ -0,0 +1,70 @@
+import React, { useState } from 'react';
+import * as Icons from 'react-feather';
+import './MyDonations.scss';
+import Hamburger from '../components/Hamburger';
+import { motion, AnimatePresence } from 'framer-motion';
+const MyDonations = ({ deleteButton }) => {
+    const [ hamburger, sethamburger ] = useState(false);
+
+    const fRaisers = [
+        {
+            title: 'This is the cause',
+            donation: 1200
+        },
+        {
+            title: 'This is the cause',
+            donation: 1200
+        },
+        {
+            title: 'This is the cause',
+            donation: 1200
+        },
+        {
+            title: 'This is the cause',
+            donation: 1200
+        }
+    ];
+
+    return (
+        <div
+            className='MyDonations'
+            onClick={() => {
+                hamburger && sethamburger(false);
+            }}
+        >
+            <div className='hamburger-button'>
+                <Icons.Menu
+                    onClick={() => {
+                        sethamburger(true);
+                    }}
+                />
+                <span>My Donations</span>
+            </div>
+            <AnimatePresence initial={false}>{hamburger && <Hamburger handleBurger={sethamburger} />}</AnimatePresence>
+            <div className='donations'>
+                <div className='title'>
+                    <span>{deleteButton ? 'Favorites' : 'Donations'}</span>
+                </div>
+                <div className='donation-cards'>
+                    {fRaisers.map(({ title, donation }) => (
+                        <div className='donation-card'>
+                            <div className='left'>
+                                <div className='cause'>{title}</div>
+                                <div className='donation'>
+                                    Your Donation: <span>${donation}</span>
+                                </div>
+                            </div>
+                            <div className='right'>
+                                {deleteButton ? <Icons.Trash color='red' /> : <Icons.ChevronRight />}
+                            </div>
+                        </div>
+                    ))}
+                </div>
+            </div>
+
+            <div />
+        </div>
+    );
+};
+
+export default MyDonations;
