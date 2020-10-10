30d563293dd8cfd83b0ddde382b38e8266df3c95
sushantbaskota2 <sushantbaskota2@gmail.com>
2020-10-10 05:41:09 -0500

    fixed profile

--- src/containers/MyProfile.js
+++ src/containers/MyProfile.js
@@ -18,7 +18,6 @@ const MyProfile = ({ history, hamburger, setHamBurger }) => {
         });
         return () => {};
     }, []);
-    console.log(user);
 
     return (
         user && (
@@ -31,14 +30,14 @@ const MyProfile = ({ history, hamburger, setHamBurger }) => {
                 <HamburgerButton setHamBurger={setHamBurger} />
                 <div className='banner'>
                     <div className='avatar-overlay' />
-                    <img src={`http://locahost:8000${user.avatar}`} />
+                    <img src={`http://locahost:8000/${user.avatar}`} />
                 </div>
                 <div className='sexyDiv'>
                     <div className='avatar'>
                         <span className='add'>
                             <Icons.PlusCircle color='white' />
                         </span>
-                        <img src={`http://locahost:8000${user.avatar}`} />
+                        <img src={`http://locahost:8000/${user.avatar}`} />
                     </div>
                     <div className='top'>
                         <span className='name'>{user.name}</span>
