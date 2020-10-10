12b1f64de078bdd2f16d00c7ebd1c40fc21a974c
sushantbaskota2 <sushantbaskota2@gmail.com>
2020-10-10 01:52:03 -0500

    Fixed minor issues

--- src/containers/MyDonations.js
+++ src/containers/MyDonations.js
@@ -1,87 +1,131 @@
-import React, { useState, useEffect } from "react";
-import * as Icons from "react-feather";
+import React, { useState, useEffect } from 'react';
+import * as Icons from 'react-feather';
+import moment from 'moment';
+import axios from '../axios';
+import ContentLoader from '../components/ContentLoader';
 
-import axios from "../axios";
-import ContentLoader from "../components/ContentLoader";
+const MyDonations = ({ deleteButton, hamburger, setHamBurger, history }) => {
+    const [ donations, setdonations ] = useState([]);
+    const [ loading, setloading ] = useState(true);
+    const [ deleteDono, setdelete ] = useState(false);
+    useEffect(
+        () => {
+            setloading(true);
+            try {
+                if (deleteButton) {
+                    axios.get('users/favorites').then((data) => {
+                        setdonations(data.data);
+                        console.log(data.data);
+                    });
+                    setloading(false);
+                } else {
+                    axios.get('users/me/donations/').then((data) => {
+                        setdonations(data.data);
+                    });
+                    setloading(false);
+                }
+            } catch (e) {
+                console.log(e);
+                setloading(false);
+            }
+        },
+        [ deleteDono ]
+    );
 
-const MyDonations = ({ deleteButton, hamburger, setHamBurger }) => {
-  const [users, setusers] = useState(null);
-  console.log(users);
-  // useEffect(async () => {
-  //   console.log("hero");
+    return (
+        <div
+            className='MyDonations'
+            onClick={() => {
+                hamburger && setHamBurger(false);
+            }}
+        >
+            <div className='hamburger-button'>
+                <Icons.Menu
+                    onClick={() => {
+                        setHamBurger(true);
+                    }}
+                />
+                <span>My {deleteButton ? 'Favorites' : 'Donations'}</span>
+            </div>
 
-  //   const { data } = await axios.get("/users/");
-  //   console.log(data);
-  //   setusers(data);
-  // }, []);
-
-  const fRaisers = [
-    {
-      title: "This is the cause",
-      donation: 1200,
-    },
-    {
-      title: "This is the cause",
-      donation: 1200,
-    },
-    {
-      title: "This is the cause",
-      donation: 1200,
-    },
-    {
-      title: "This is the cause",
-      donation: 1200,
-    },
-  ];
-
-  return (
-    <div
-      className="MyDonations"
-      onClick={() => {
-        hamburger && setHamBurger(false);
-      }}
-    >
-      <div className="hamburger-button">
-        <Icons.Menu
-          onClick={() => {
-            setHamBurger(true);
-          }}
-        />
-        <span>My {deleteButton ? "Favorites" : "Donations"}</span>
-      </div>
-
-      <div className="donations">
-        <div className="title">
-          <span>{deleteButton ? "Favorites" : "Donations"}</span>
-        </div>
-        <div className="donation-cards">
-          {!users ? (
-            <ContentLoader />
-          ) : (
-            fRaisers.map(({ title, donation }) => (
-              <div className="donation-card">
-                <div className="left">
-                  <div className="cause">{title}</div>
-                  <div className="donation">
-                    Your Donation: <span>${donation}</span>
-                  </div>
+            <div className='donations'>
+                <div className='title'>
+                    <span>{deleteButton ? 'Favorites' : 'Donations'}</span>
                 </div>
-                <div className="right">
-                  {deleteButton ? (
-                    <Icons.Trash color="red" />
-                  ) : (
-                    <Icons.ChevronRight />
-                  )}
-                </div>
-              </div>
-            ))
-          )}
-        </div>
-      </div>
+                <div className='donation-cards'>
+                    {loading ? (
+                        <ContentLoader />
+                    ) : (
+                        donations.map((hero) => {
+                            console.log(donations);
+                            if (deleteButton) {
+                                const { cause: { title, _id, endDate }, amount: donation, _id: dID } = hero;
+                                return (
+                                    <div className='donation-card' onClick={() => history.push(`/campaign/${_id}`)}>
+                                        <div className='left'>
+                                            <div className='cause'>{title}</div>
+                                            {!deleteButton ? (
+                                                <div className='donation'>
+                                                    Your Donation: <span>${donation}</span>
+                                                </div>
+                                            ) : (
+                                                <div className='donation'>
+                                                    End Date: {moment(endDate).format('MMM DD, YYYY')}
+                                                </div>
+                                            )}
+                                        </div>
+                                        <div className='right'>
+                                            {deleteButton ? (
+                                                <Icons.Trash
+                                                    color='red'
+                                                    onClick={async (e) => {
+                                                        e.stopPropagation();
+                                                        try {
+                                                            setdelete(!deleteDono);
+                                                            await axios.delete(`/users/favorites/${dID}`);
 
-      <div />
-    </div>
-  );
+                                                            setdonations(
+                                                                [ ...donations ].filter(
+                                                                    ({ _id }) => _id.toString() !== dID
+                                                                )
+                                                            );
+                                                        } catch (error) {
+                                                            console.log('couldnt delete');
+                                                        }
+                                                    }}
+                                                />
+                                            ) : (
+                                                <Icons.ChevronRight />
+                                            )}
+                                        </div>
+                                    </div>
+                                );
+                            } else {
+                                const { cause: { title, _id: cID }, amount: donation, _id } = hero;
+                                return (
+                                    <div className='donation-card' onClick={() => history.push(`/campaign/${cID}`)}>
+                                        <div className='left'>
+                                            <div className='cause'>{title}</div>
+                                            {
+                                                <div className='donation'>
+                                                    Your Donation: <span>${donation}</span>
+                                                </div>
+                                            }
+                                        </div>
+                                        <div className='right'>
+                                            <Icons.ChevronRight />
+                                        </div>
+                                    </div>
+                                );
+                            }
+                        })
+                    )}
+                </div>
+            </div>
+
+            <div />
+        </div>
+    );
 };
 
 export default MyDonations;
--- src/containers/MyFundraiser.js
+++ src/containers/MyFundraiser.js
@@ -1,86 +1,86 @@
-import React, { useState, useEffect } from "react";
-import HamburgerButton from "../components/HamburgerButton";
-import moment from "moment";
-import Slider from "../components/Slider";
-import axios from "../axios";
-import ContentLoaderP from "../components/ContentLoader";
-const MyFundraiser = ({ hamburger, setHamBurger }) => {
-  const [fundraisers, setFundraisers] = useState(null);
-  const [loading, setloading] = useState(true);
+import React, { useState, useEffect } from 'react';
+import HamburgerButton from '../components/HamburgerButton';
+import moment from 'moment';
+import Slider from '../components/Slider';
+import axios from '../axios';
+import ContentLoaderP from '../components/ContentLoader';
+const MyFundraiser = ({ hamburger, setHamBurger, history }) => {
+    const [ fundraisers, setFundraisers ] = useState([]);
+    const [ loading, setloading ] = useState(true);
 
-  useEffect(() => {
-    try {
-      setloading(true);
-      axios.get("/causes/me").then(({ data }) => {
-        setFundraisers(data);
-        setloading(false);
-      });
-    } catch (error) {
-      setloading(false);
-    }
-  }, []);
+    useEffect(() => {
+        try {
+            setloading(true);
 
-  return (
-    <div
-      className="MyFundraisers"
-      onClick={hamburger ? () => setHamBurger(false) : () => {}}
-    >
-      <HamburgerButton setHamBurger={setHamBurger} title="My Fundraisers" />
+            axios.get('/causes/me').then(({ data }) => {
+                setFundraisers(data);
+                console.log(data);
+                setloading(false);
+            });
+        } catch (error) {
+            setloading(false);
+        }
+    }, []);
 
-      <div className="title">
-        <span>Fundraisers</span>
-      </div>
+    return (
+        <div className='MyFundraisers' onClick={hamburger ? () => setHamBurger(false) : () => {}}>
+            <HamburgerButton setHamBurger={setHamBurger} title='My Fundraisers' />
 
-      {loading ? (
-        <ContentLoaderP />
-      ) : (
-        <div className="fund-cards">
-          {fundraisers.map(
-            ({ endDate, title, images, donations, goal, _id }) => {
-              return (
-                <FundCard
-                  key={_id}
-                  endDate={endDate}
-                  title={title}
-                  src={images[0].image}
-                  raised={
-                    donations.length === 0
-                      ? 0
-                      : donations.reduce((a, c) => a + c.amount, 0)
-                  }
-                  goal={goal}
-                  i={_id}
-                />
-              );
-            }
-          )}
+            <div className='title'>
+                <span>Fundraisers</span>
+            </div>
+
+            {loading ? (
+                <ContentLoaderP />
+            ) : (
+                <div className='fund-cards'>
+                    {fundraisers.map(({ endDate, title, images, donations, goal, _id }) => {
+                        return (
+                            <FundCard
+                                key={_id}
+                                endDate={endDate}
+                                title={title}
+                                src={images.length > 0 ? images[0].image : ''}
+                                raised={donations.length === 0 ? 0 : donations.reduce((a, c) => a + c.amount, 0)}
+                                goal={goal}
+                                i={_id}
+                                history={history}
+                            />
+                        );
+                    })}
+                </div>
+            )}
         </div>
-      )}
-    </div>
-  );
+    );
 };
 
-const FundCard = ({ title, endDate, src, raised, goal, i }) => (
-  <div key={i} className="fund-card">
-    <div className="left">
-      <img src={`http://localhost:8000${src}`} />
-    </div>
-    <div className="middle">
-      <span>{title}</span>
+const FundCard = ({ title, endDate, history, src, raised, goal, i }) => (
+    <div
+        key={i}
+        className='fund-card'
+        onClick={() => {
+            history.push(`/campaign/${i}`);
+        }}
+    >
+        <div className='left'>
+            <img src={`http://localhost:8000${src}`} />
+        </div>
+        <div className='middle'>
+            <span>{title}</span>
 
-      {<Slider raised={raised} goal={goal} />}
-      <div className="two">
-        <span>{raised}</span>
-        <span style={{ color: "black" }}>of</span>
-        <span>{goal}</span>
-      </div>
-    </div>
+            {<Slider raised={raised} goal={goal} />}
+            <div className='two'>
+                <span>{raised}</span>
+                <span style={{ color: 'black' }}>of</span>
+                <span>{goal}</span>
+            </div>
+        </div>
 
-    <div className="end">
-      <span>Ends on:</span>
-      <span className="end-date"> {moment(endDate).format("MMM D, YYYY")}</span>
+        <div className='end'>
+            <span>Ends on:</span>
+            <span className='end-date'> {moment(endDate).format('MMM D, YYYY')}</span>
+        </div>
     </div>
-  </div>
 );
 
 export default MyFundraiser;
