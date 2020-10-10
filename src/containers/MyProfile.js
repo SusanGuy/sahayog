b454bcd0847b592babc699c93885c5202d537fb2
Susan Subedi <ssubedi1@go.olemiss.edu>
2020-10-10 07:39:41 -0500

    My profile fixed

--- src/containers/MyProfile.js
+++ src/containers/MyProfile.js
@@ -1,76 +1,104 @@
-import React, { useEffect, useState } from 'react';
-import './MyProfile.scss';
-import * as Icons from 'react-feather';
-import HamburgerButton from '../components/HamburgerButton';
-import axios from '../axios';
+import React, { useEffect, useState } from "react";
+import "./MyProfile.scss";
+import * as Icons from "react-feather";
+import HamburgerButton from "../components/HamburgerButton";
+import axios from "../axios";
+import { connect } from "react-redux";
 
-const MyProfile = ({ history, hamburger, setHamBurger }) => {
-    const [ user, setuser ] = useState(null);
-    const [ donation, setDonation ] = useState(0);
-    const [ loading, setloading ] = useState(true);
-    useEffect(() => {
-        axios.get('/users/me').then(({ data }) => {
-            setuser(data);
-        });
+const MyProfile = ({ history, hamburger, setHamBurger, logout, user }) => {
+  const [donation, setDonation] = useState(0);
 
-        axios.get('/users/me/donations').then(({ data }) => {
-            setDonation(data.length);
-        });
-        return () => {};
-    }, []);
+  useEffect(() => {
+    axios.get("/users/me/donations").then(({ data }) => {
+      setDonation(data.length);
+    });
+    return () => {};
+  }, []);
 
-    return (
-        user && (
-            <div
-                className='MyProfile'
-                onClick={(e) => {
-                    return hamburger && setHamBurger(false);
-                }}
+  const [avatar, setAvatar] = useState(user && user.avatar);
+
+  const uploadImage = async (image) => {
+    try {
+      const fd = new FormData();
+      fd.append("upload", image, image.name);
+      await axios.post("/users/me/avatar", fd);
+      const avat = await axios.get(`/users/${user._id}/avatar`);
+      setAvatar(avat.data);
+    } catch (error) {}
+  };
+
+  console.log(avatar);
+
+  return (
+    user && (
+      <div
+        className="MyProfile"
+        onClick={(e) => {
+          return hamburger && setHamBurger(false);
+        }}
+      >
+        <HamburgerButton setHamBurger={setHamBurger} />
+        <div className="banner">
+          <div className="avatar-overlay" />
+          <img src={`http://localhost:8000${avatar}`} />
+        </div>
+        <div className="sexyDiv">
+          <div className="avatar">
+            <input
+              id="chooseFile"
+              type="file"
+              onChange={(e) => uploadImage(e.target.files[0])}
+              name="upload"
+              style={{ display: "none" }}
+              accept="image/*"
+            />
+            <span
+              onClick={() => document.getElementById("chooseFile").click()}
+              className="add"
             >
-                <HamburgerButton setHamBurger={setHamBurger} />
-                <div className='banner'>
-                    <div className='avatar-overlay' />
-                    <img src={`http://localhost:8000${user.avatar}`} />
-                </div>
-                <div className='sexyDiv'>
-                    <div className='avatar'>
-                        <span className='add'>
-                            <Icons.PlusCircle color='white' />
-                        </span>
-                        <img src={`http://localhost:8000${user.avatar}`} />
-                    </div>
-                    <div className='top'>
-                        <span className='name'>{user.name}</span>
+              <Icons.PlusCircle color="white" />
+            </span>
+            <img src={`http://localhost:8000${avatar}`} />
+          </div>
+          <div className="top">
+            <span className="name">{user.name}</span>
 
-                        <div className='couple-couple'>
-                            <div className='couple'>
-                                <div className='number'>{user.favorites.length}</div>
-                                <div>Favorites</div>
-                            </div>
+            <div className="couple-couple">
+              <div className="couple">
+                <div className="number">{user.favorites.length}</div>
+                <div>Favorites</div>
+              </div>
 
-                            <div className='couple'>
-                                <div className='number'>{donation}</div>
-                                <div>Donations</div>
-                            </div>
-                        </div>
-                    </div>
-                    <hr />
-
-                    <div className='bottom'>
-                        <div className='bottom-card'>
-                            <span>Change Password</span>
-                        </div>
-                        <div className='bottom-card' onClick={() => history.push('my-fundraisers')}>
-                            <span>My Fundraisers</span>
-                        </div>
-                        <div className='bottom-card' onClick={() => history.push('/logout')}>
-                            <span style={{ color: 'red' }}>Logout</span>
-                        </div>
-                    </div>
-                </div>
+              <div className="couple">
+                <div className="number">{donation}</div>
+                <div>Donations</div>
+              </div>
             </div>
-        )
-    );
+          </div>
+          <hr />
+
+          <div className="bottom">
+            <div className="bottom-card">
+              <span>Change Password</span>
+            </div>
+            <div
+              className="bottom-card"
+              onClick={() => history.push("my-fundraisers")}
+            >
+              <span>My Fundraisers</span>
+            </div>
+            <div className="bottom-card" onClick={() => logout()}>
+              <span style={{ color: "red" }}>Logout</span>
+            </div>
+          </div>
+        </div>
+      </div>
+    )
+  );
 };
 
-export default MyProfile;
+const mapStateToProps = (state) => {
+  return { user: state.auth.user };
+};
+
+export default connect(mapStateToProps)(MyProfile);
