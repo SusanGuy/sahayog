ca3b7d799a2400640f4fa7c807c1aeefed725f60
sushantbaskota2 <sushantbaskota2@gmail.com>
2020-10-09 21:13:51 -0500

    fixed home loader

--- src/components/ContentLoader.js
+++ src/components/ContentLoader.js
@@ -24,15 +24,31 @@ const ContentLoaderP = () => {
 export const ContentLoaderHome = () => {
     return (
         <>
-            <ContentLoader viewBox='0 0 400 475' height={475} width={'100%'} style={{ marginTop: '8rem' }}>
+        <div>
+            <ContentLoader viewBox='0 0 400 475' height={400} style={{ marginTop: '8rem',height:'100%' }}>
                 <circle cx='50' cy='308' r='30' />
                 <rect x='125' y='283' rx='4' ry='4' width='100' height='13' />
                 <rect x='125' y='310' rx='4' ry='4' width='50' height='8' />
                 <rect x='25' y='2' rx='5' ry='5' width='60%' height='10' />
                 <rect x='25' y='0' rx='5' ry='5' width='60%' height='250' />
-            </ContentLoader>
 
-        </>
+            
+            </ContentLoader>
+            
+        </div>
+         <div style={{margin:0, padding:0}}>
+            <ContentLoader viewBox='0 0 400 475' height={400} style={{ marginTop: '8rem',height:'100%'  }}>
+                <circle cx='50' cy='308' r='30' />
+                <rect x='125' y='283' rx='4' ry='4' width='100' height='13' />
+                <rect x='125' y='310' rx='4' ry='4' width='50' height='8' />
+                <rect x='25' y='2' rx='5' ry='5' width='60%' height='10' />
+                <rect x='25' y='0' rx='5' ry='5' width='60%' height='250' />
+
+            
+            </ContentLoader>
+        </div>
+             
+         </>
     );
 };
 
